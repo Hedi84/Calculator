6bffd1850d10999a02b36de031b3a522789ff28e
Hedi Van Meurs <alisoun@gmail.com>
2018-10-29 16:15:22 +0000

    refractoring

--- index.js
+++ index.js
@@ -1,69 +1,86 @@
 const readline = require('readline-sync');
 
-console.log('Welcome to the calculator!');
-console.log('Please enter the operator:');
-const operator = readline.prompt();
-// create an array for the numbers
-const numbersArray = []
-// turn each string into a number before adding it to the array
-console.log('Please enter your first number:');
-numbersArray.push(Number(readline.prompt()));
-console.log('Please enter your second number:');
-numbersArray.push(Number(readline.prompt()));
-console.log(numbersArray)
-// create a loop that keeps asking for a number and breaks upon entering case
-let input;
-do {
-  console.log('Please enter another number or enter c to calculate:');
-  input = readline.prompt();
-  if (input != 'c') {
-    numbersArray.push(parseFloat(input));
-  }
-} while (input != 'c');
-
-let outcome = 0;
-
-switch (operator) {
-  case '+':
-    numbersArray.forEach(function(number){
-    outcome += number;
-    });
-    break;
-  case '-':
-    numbersArray.forEach(function(number){
-    outcome -= number;
-    });
-    break;
-  case '*':
-    outcome = 1
-    numbersArray.forEach(function(number){
-    outcome *= number;
-    });
-    break;
-  case '/':
-    outcome = numbersArray[0]
-    numbersArray.splice(0, 1)
-    numbersArray.forEach(function(number){
-    outcome /= number;
-    });
-    break;
-  default:
-    console.log('Sorry, that\'s not a valid operator');
+function askOperator() {
+  console.log('Please enter the operator:');
+  const operator = readline.prompt();
+  return operator
 }
 
-console.log(outcome);
+function askNumbers() {
+  // create an array for the numbers
+  let numbersArray = []
+  // turn each string into a number before adding it to the array
+  // console.log('Please enter your first number:');
+  // numbersArray.push(Number(readline.prompt()));
+  // console.log('Please enter your second number:');
+  // numbersArray.push(Number(readline.prompt()));
+  // console.log(numbersArray);
+  let input;
+  do {
+    console.log('Please enter a number or enter c to calculate:');
+    input = readline.prompt();
+    // if input is c break the loop
+    if (input === 'c') {
+      break
+    // if input is not a number, don't add to numbersArray
+  } else if (isNaN(Number(input))) {
+      console.log('That is not a valid number, try again');
+      console.log(numbersArray);
+    // if input is a number, add to array
+    } else {
+      numbersArray.push(parseFloat(input));
+      console.log(numbersArray);
+    }
+  } while (input != 'c');
+  return  numbersArray
+}
 
-//   console.log(number_one + number_two)
-//   break;
-// case '-':
-//   console.log(number_one - number_two)
-//   break;
-// case '*':
-//   console.log(number_one * number_two)
-//   break;
-// case '/':
-//   console.log(number_one / number_two)
-//   break;
+function performOneCalculation(operator) {
+  // create a loop that keeps asking for a number and breaks upon entering case
+  let numbersArray = askNumbers();
+  applyOperator(numbersArray);
+}
 
-// const number_one = Number(first_number_string)
-// const number_two = Number(second_number_string);
+function applyOperator(array) {
+  const operator = askOperator()
+  let outcome = array[0]
+  array.splice(0, 1)
+  switch (operator) {
+    case '+':
+      array.forEach(function(number){
+      outcome += number;
+      });
+      break;
+    case '-':
+      array.forEach(function(number){
+      outcome -= number;
+      });
+      break;
+    case '*':
+      array.forEach(function(number){
+      outcome *= number;
+      });
+      break;
+    case '/':
+      array.forEach(function(number){
+      outcome /= number;
+      });
+      break;
+    default:
+      console.log('Sorry, that\'s not a valid operator');
+  }
+  console.log(outcome);
+}
+
+function runCalculator() {
+  let answer = 'y'
+  console.log('Welcome to the calculator!');
+  do {
+    performOneCalculation();
+    console.log('Would you like to do another calculation? y/n')
+    answer = readline.prompt();
+  } while (answer != 'n');
+  console.log('Toodles!');
+}
+
+runCalculator()
