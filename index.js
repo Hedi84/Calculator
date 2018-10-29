e3242e81a7617c06b1e9692393d0a162560c0a46
Hedi Van Meurs <alisoun@gmail.com>
2018-10-29 12:40:05 +0000

    calculator works

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const readline = require('readline-sync');
 
 console.log('Welcome to the calculator!');
+console.log('Please enter the operator:');
+const operator = readline.prompt();
 console.log('Please enter your first number:');
-const first_number = readline.prompt();
+const first_number_string = readline.prompt();
 console.log('Please enter your first number:');
-const second_number = readline.prompt();
-const result = parseInt(first_number, 10)*parseInt(second_number, 10);
-console.log(result);
+const second_number_string = readline.prompt();
+
+const number_one = Number(first_number_string)
+const number_two = Number(second_number_string);
+
+switch (operator) {
+  case '+':
+    console.log(number_one + number_two)
+    break;
+  case '-':
+    console.log(number_one - number_two)
+    break;
+  case '*':
+    console.log(number_one * number_two)
+    break;
+  case '/':
+    console.log(number_one / number_two)
+    break;
+  default:
+    console.log('Sorry, that\'s not a valid operator');
+}
