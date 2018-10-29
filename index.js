e69003068f2b1f0452875fe7439b3ada3de61069
Hedi Van Meurs <alisoun@gmail.com>
2018-10-29 12:02:01 +0000

    mulitplies 2 numbers

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const readline = require('readline-sync');
 
 console.log('Welcome to the calculator!');
-console.log('Please enter some input:');
-const response = readline.prompt();
+console.log('Please enter your first number:');
+const first_number = readline.prompt();
+console.log('Please enter your first number:');
+const second_number = readline.prompt();
+const result = parseInt(first_number, 10)*parseInt(second_number, 10);
+console.log(result);
