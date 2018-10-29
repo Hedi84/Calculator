b1dd3b6e450692a3db65b25b09182c6b357df252
Hedi Van Meurs <alisoun@gmail.com>
2018-10-29 17:49:03 +0000

    vowel calculator

--- index.js
+++ index.js
@@ -69,18 +69,90 @@ function applyOperator(array) {
     default:
       console.log('Sorry, that\'s not a valid operator');
   }
-  console.log(outcome);
+  console.log('Your outcome is' + ' ' + outcome);
 }
 
 function runCalculator() {
-  let answer = 'y'
-  console.log('Welcome to the calculator!');
-  do {
+  let answer = 'y';
+  console.log('Welcome to the arithmetic calculator!');
+  while (answer != 'n'){
     performOneCalculation();
-    console.log('Would you like to do another calculation? y/n')
+    console.log('Would you like to do another calculation? y/n');
     answer = readline.prompt();
-  } while (answer != 'n');
+    if (answer == 'n') {
+      break;
+    }
+  }
   console.log('Toodles!');
 }
 
-runCalculator()
+function printWelcomeMessage () {
+  console.log('Welcome to the calculator!');
+  console.log('==========================');
+  console.log('Which calculator mode do you want?');
+  console.log(' 1) Arithmetic');
+  console.log(' 2) Vowel counting');
+}
+
+function userInterface () {
+  let choice
+  printWelcomeMessage();
+  while (choice !== '1' && choice !== '2') {
+    console.log('Please enter 1 or 2');
+    choice = readline.prompt();
+  }
+  if (choice === '1') {
+    runCalculator();
+  } else {
+    vowelCalculator();
+  }
+}
+
+function askUserString() {
+  console.log('Please enter text to count vowels:')
+  let string = readline.prompt();
+  string = string.toLowerCase();
+  //  function that counts vowels and returns the counts
+  countVowels(string);
+}
+
+function countVowels(string) {
+  const a = string.match(/[a]/gi);
+  answer_a = a === null ? 0 : a.length;
+  console.log("A: " + answer_a);
+  const e = string.match(/[e]/gi);
+  answer_e = e === null ? 0 : e.length;
+  console.log("E: " + answer_e);
+  const i = string.match(/[i]/gi);
+  answer_i = i === null ? 0 : i.length;
+  console.log("I: " + answer_i);
+  const o = string.match(/[o]/gi);
+  answer_o = o === null ? 0 : o.length;
+  console.log("O: " + answer_o);
+  const u = string.match(/[u]/gi);
+  answer_u = u === null ? 0 : u.length;
+  console.log("U: " + answer_u);
+}
+
+function vowelCalculator() {
+  // function that asks user for string and stores it in an array
+  askUserString()
+  // ask user if they want to count again or quit
+  askUserContinue()
+}
+
+function askUserContinue() {
+  console.log("What would you like to do now?\n\
+  1) Calculate another string\n\
+  2) Back to main menu \n\
+  3) Enter any character to quit")
+  choice = readline.prompt();
+  if (choice === '1') {
+    vowelCalculator()
+  } else if (choice === '2') {
+    userInterface()
+  } else
+    console.log('cya')
+}
+
+userInterface()
