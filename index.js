60156f27a332a50b46c8a432b449e1e57939cdec
Hedi Van Meurs <alisoun@gmail.com>
2018-10-29 14:40:57 +0000

    calculates

--- index.js
+++ index.js
@@ -3,27 +3,67 @@ const readline = require('readline-sync');
 console.log('Welcome to the calculator!');
 console.log('Please enter the operator:');
 const operator = readline.prompt();
+// create an array for the numbers
+const numbersArray = []
+// turn each string into a number before adding it to the array
 console.log('Please enter your first number:');
-const first_number_string = readline.prompt();
-console.log('Please enter your first number:');
-const second_number_string = readline.prompt();
+numbersArray.push(Number(readline.prompt()));
+console.log('Please enter your second number:');
+numbersArray.push(Number(readline.prompt()));
+console.log(numbersArray)
+// create a loop that keeps asking for a number and breaks upon entering case
+let input;
+do {
+  console.log('Please enter another number or enter c to calculate:');
+  input = readline.prompt();
+  if (input != 'c') {
+    numbersArray.push(parseFloat(input));
+  }
+} while (input != 'c');
 
-const number_one = Number(first_number_string)
-const number_two = Number(second_number_string);
+let outcome = 0;
 
 switch (operator) {
   case '+':
-    console.log(number_one + number_two)
+    numbersArray.forEach(function(number){
+    outcome += number;
+    });
     break;
   case '-':
-    console.log(number_one - number_two)
+    numbersArray.forEach(function(number){
+    outcome -= number;
+    });
     break;
   case '*':
-    console.log(number_one * number_two)
+    outcome = 1
+    numbersArray.forEach(function(number){
+    outcome *= number;
+    });
     break;
   case '/':
-    console.log(number_one / number_two)
+    outcome = numbersArray[0]
+    numbersArray.splice(0, 1)
+    numbersArray.forEach(function(number){
+    outcome /= number;
+    });
     break;
   default:
     console.log('Sorry, that\'s not a valid operator');
 }
+
+console.log(outcome);
+
+//   console.log(number_one + number_two)
+//   break;
+// case '-':
+//   console.log(number_one - number_two)
+//   break;
+// case '*':
+//   console.log(number_one * number_two)
+//   break;
+// case '/':
+//   console.log(number_one / number_two)
+//   break;
+
+// const number_one = Number(first_number_string)
+// const number_two = Number(second_number_string);
